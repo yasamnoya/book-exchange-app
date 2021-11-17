356971c4b13c30b8f9d1923790421e3ce675199f
yasamnoya <yasamnono@gmail.com>
2021-11-17 14:30:25 +0800

    add trade model

--- src/models/index.js
+++ src/models/index.js
@@ -2,4 +2,5 @@ module.exports = {
   Book: require('./book.model'),
   User: require('./user.model'),
   Request: require('./request.model'),
+  Trade: require('./trade.model'),
 };
---
+++ src/models/trade.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const tradeSchema = new mongoose.Schema({
+  getOne: {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    books: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+        required: true,
+      },
+    ],
+  },
+  getTwo: {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    books: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+        required: true,
+      },
+    ],
+  },
+});
+
+const Trade = mongoose.model(tradeSchema);
+module.exports = Trade;
